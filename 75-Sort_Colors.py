43bda904333553ac61d5e7a3ef68c02ad5df3f07
Yang Xiao <swagshaw1998@gmail.com>
2022-04-07 15:53:41 +0800

    :tada: 75-Sort_Colors.py

---
+++ 75-Sort_Colors.py
@@ -0,0 +1,30 @@
+"""
+# !/usr/bin/env python
+-*- coding: utf-8 -*-
+@Time    : 2022/4/7 下午3:44
+@Author  : Yang "Jan" Xiao 
+@Description : 75-Sort_Colors
+"""
+
+
+class Solution:
+    def sortColors(self, nums):
+        red, white, blue = 0, 0, len(nums) - 1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    test = [2, 0, 2, 1, 1, 0]
+    solution.sortColors(test)
+    print(test)
